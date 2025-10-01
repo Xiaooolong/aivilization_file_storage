4fa3bd68bd27f7e2c39bf3350ae90b4227ac03d9
Xiaooolong <xwangid@connect.ust.hk>
2025-10-01 23:42:27 +0800

    test

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ import jwt
 import datetime
 
 # ===== 配置区 =====
-PRIVATE_KEY_PATH = r"C:\Users\xiaoo\Desktop\jwt-keys\private.pem"   # 你的私钥路径
+PRIVATE_KEY_PATH = "/Users/countingsheep/Projects/Unity/private.pem"   # 你的私钥路径
 CHARACTER_ID = "693595"            # 测试用的角色 ID
 CHARACTER_ID_FAKE = "999999"       # 用于测试 characterId 不匹配的 ID
-API_URL = f"http://127.0.0.1:8001/characters/{CHARACTER_ID_FAKE}/sas"  # 本地 FastAPI 服务地址
+# API_URL = f"http://127.0.0.1:8001/characters/{CHARACTER_ID_FAKE}/sas"  # 本地 FastAPI 服务地址
+
+API_URL = f"https://api.aivilization.ai/fileStorage/characters/{CHARACTER_ID}/sas"  # 本地 FastAPI 服务地址
 
 # ===== 生成 JWT token =====
 with open(PRIVATE_KEY_PATH, "rb") as f:
@@ -14,7 +16,7 @@ with open(PRIVATE_KEY_PATH, "rb") as f:
 
 payload = {
     "userId": "test-user-123",
-    "characterId": CHARACTER_ID,
+    "characterId": 69359577,
     "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
     "iat": datetime.datetime.utcnow(),
     "iss": "Game Server",
@@ -22,14 +24,15 @@ payload = {
 }
 
 token = jwt.encode(payload, private_key, algorithm="RS256")
-
+print("✅ 生成的 JWT Token:", token)
 # ===== 调用 API =====
-headers = {
-    "Authentication": f"Bearer {token}"
-}
+# headers = {
+#     "Authentication": f"Bearer {token}"
+# }
 
-resp = requests.get(API_URL, headers=headers)
+# resp = requests.get(API_URL, headers=headers)
 
-print("✅ 请求 URL:", API_URL)
-print("✅ 返回状态码:", resp.status_code)
-print("✅ 返回内容:", resp.text)
+# print("✅ 请求 URL:", API_URL)
+# print("✅ 返回状态码:", resp.status_code)
+# print("✅ 返回内容:", resp.text)
+# https://aivilization-stage-report.onrender.com/?lang=en&char_id=69359577&token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ0ZXN0LXVzZXItMTIzIiwiY2hhcmFjdGVySWQiOjY5MzU5NTc3LCJleHAiOjE3NTkzMjEyOTgsImlhdCI6MTc1OTMxNzY5OCwiaXNzIjoiR2FtZSBTZXJ2ZXIiLCJhdWQiOiJ1c2VyX2lkX2hlcmUifQ.CczFEFxJyaDNK3uyyjSZIZgS6wRXJHaSp37z_D07zHO7qjhDrEd-os5ucm_hATvM25VNMHFrqbhM_nm7R7eJVAyKFnsdA620f_W6CDcsywfKNLafVUHvxkcV5dfiu7Oy1Ju6NqMBHxXLygL6DavbiGQeYZVvBtW3E6b8gpvsSZLAnYIZgOCSjWaqt10LUkQSjMNLU6zntSt9f2qiZFhqqcA1oNSVP-GFgtj7A7wT05DlwIgQ8wuWBpkPsK7OFeIM1FNz5cnUfNv1x5lml_yakJteoLXTUZU6CNFFXaTUFfbp4UMauhbZL2qrU31RUt2NZTP4zOzUqB2yQncSrFiPUw
